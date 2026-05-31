20cabe0b16ed8c7c4dd93dcd8b27d7d4b6407f61
hannes557766-ui <226123776+hannes557766-ui@users.noreply.github.com>
2026-05-31 19:10:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,66 +1,22 @@
 const CACHE_NAME = 'kalorify-v1';
 
-// Diese Dateien werden beim ersten Start auf das Handy geladen und gespeichert
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Syne:wght@700;800&family=DM+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap',
-  'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js'
-];
-
-// Installation: Cachen der Basis-Dateien
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Service Worker: Caching Dateien');
-        return cache.addAll(ASSETS_TO_CACHE);
-      })
-  );
+self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-// Aktivierung: Alte Caches aufräumen, falls du später Updates machst
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Service Worker: Alter Cache gelöscht', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
 
-// Fetch: Das Handy fragt nach einer Datei (z.B. index.html)
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('./'))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request)
-      .then((cachedResponse) => {
-        // Wenn wir die Datei gespeichert haben, gib sie sofort zurück (Offline-Support)
-        if (cachedResponse) {
-          // Im Hintergrund versuchen wir trotzdem die neuste Version aus dem Netz zu laden
-          fetch(event.request).then((networkResponse) => {
-            caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, networkResponse.clone());
-            });
-          }).catch(() => {}); // Wenn kein Internet da ist, ignoriere den Fehler
-          return cachedResponse;
-        }
-        
-        // Wenn die Datei nicht im Cache ist, lade sie normal aus dem Internet
-        return fetch(event.request).then((networkResponse) => {
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
-        });
-      })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
